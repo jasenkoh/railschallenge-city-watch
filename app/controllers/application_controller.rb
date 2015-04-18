23659d3ea800763aa65fcd5bcde61f1ccf630c04
jasenko <jasenko.hadziomeragic@virginpulse.com>
2015-04-18 16:51:10 -0400

    make render_not_found public method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,11 @@
 class ApplicationController < ActionController::Base
 	rescue_from ActionController::UnpermittedParameters, with: :render_unpermitted_parameters
 
-	protected
 	def render_not_found
 		render :json => {:message => 'page not found'}, :status => :not_found
 	end
 
+	protected
 	def render_unpermitted_parameters(exception)
 		render json: { :message => exception.message }, status: 422
 	end
