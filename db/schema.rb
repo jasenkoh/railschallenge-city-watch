99cf54b6d5200dddd2640df58a00add979fe5712
jasenko <jasenko.hadziomeragic@virginpulse.com>
2015-04-19 11:32:55 -0400

    update schema file

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150419150524) do
+ActiveRecord::Schema.define(version: 20150419152303) do
 
   create_table "emergencies", id: false, force: :cascade do |t|
     t.string   "code"
